a99c9de20ee2a871272d15ab90231ebb1d51ee6e
ThamyrisCabralArraes <thamyriscabral@hotmail.com>
2023-05-25 12:41:21 -0300

    exercicio 3 feito

--- go-bases-manha/main.go
+++ go-bases-manha/main.go
@@ -5,9 +5,16 @@ import "fmt"
 func main() {
 	var nome string = "Thamyris"
 	var idade int = 34
+
 	var temperatura int = 30
 	var umidade int = 20
 	var pressaoAtimosferica int = 29
+
+	// var sobrenome string
+	// sobrenome := 6
+	// var licenca_para_dirigir = true
+	// var estatura daPessoa int
+	// quantidadeDeFilhos := 2
 
 	fmt.Println("nome ", nome)
 	fmt.Println("idade", idade)
