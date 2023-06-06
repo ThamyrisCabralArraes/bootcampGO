a7d3d5ddbaaee6a802ccd30cbcd1d8eb76360edc
ThamyrisCabralArraes <thamyriscabral@hotmail.com>
2023-06-06 17:56:58 -0300

    criando a estrutura do service

--- exemplos/go-web-II/internal/products/service.go
+++ exemplos/go-web-II/internal/products/service.go
@@ -1,1 +1,39 @@
 package products
+
+type Service interface {
+	GetAll() ([]Product, error)
+	Store(nome, tipo string, quantidade int, preco float64) (Product, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(r Repository) Service {
+	return &service{
+		repository: r,
+	}
+
+}
+
+func (s *service) GetAll() ([]Product, error) {
+	ps, err := s.repository.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	return ps, nil
+}
+
+func (s *service) Store(nome, tipo string, quantidade int, preco float64) (Product, error) {
+	lastID, err := s.repository.lastID()
+	if err != nil {
+		return Product{}, err
+	}
+	lastID++
+
+	product, err := s.repository.Store(lastID, nome, tipo, quantidade, preco)
+	if err != nil {
+		return Product{}, err
+	}
+	return product, nil
+}
