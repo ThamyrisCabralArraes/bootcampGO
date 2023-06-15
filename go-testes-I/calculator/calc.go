775d5d7a4b8fea24bc174a2ad235b5d792b4d74b
ThamyrisCabralArraes <thamyriscabral@hotmail.com>
2023-06-15 16:17:53 -0300

    exercicio de tests feito

--- go-testes-I/calculator/calc.go
+++ go-testes-I/calculator/calc.go
@@ -1,5 +1,10 @@
 package calculator
 
+import (
+	"errors"
+	"sort"
+)
+
 // sum two values
 func Sum(n1, n2 int) int {
 	return n1 + n2
@@ -8,4 +13,18 @@ func Sum(n1, n2 int) int {
 // sub two values
 func Sub(n1, n2 int) int {
 	return n1 - n2
+}
+
+// Div two values
+func Div(n1, n2 int) (int, error) {
+	if n2 == 0 {
+		return 0, errors.New("nenhum numero pode ser divisivel por 0")
+	}
+
+	div := n1 / n2
+	return div, nil
+}
+
+func SortAscending(numbers []int) {
+	sort.Ints(numbers)
 }
--- go-testes-I/calculator/calc_test.go
+++ go-testes-I/calculator/calc_test.go
@@ -20,10 +20,52 @@ func TestSum(t *testing.T) {
 
 func TestSub(t *testing.T) {
 	num1 := 3
-	num2 := 3
-	expectedResults := 0
+	num2 := 0
+	expectedResults := 3
 
 	result := Sub(num1, num2)
 
 	assert.Equal(t, expectedResults, result, "devem ser iguais")
+}
+
+func TestDiv(t *testing.T) {
+	num1 := 3
+	num2 := 3
+	expectedResults := 1
+	// expectedError := "nenhum numero pode ser divisivel por 0"
+
+	// defer func() {
+	// 	err := recover()
+	// 	if err != nil {
+	// 		assert.Equal(t, expectedError, err, "nao pode usar zero")
+	// 	}
+	// }()
+
+	result, err := Div(num1, num2)
+	if num2 == 0 {
+		assert.Error(t, err, "nao pode usar zero")
+	}
+
+	assert.Equal(t, expectedResults, result, "devem ser iguais")
+	assert.NotNil(t, result)
+}
+
+func TestDiv2(t *testing.T) {
+	num1 := 3
+	num2 := 0
+	expectedResults := "nenhum numero pode ser divisivel por 0"
+
+	_, err := Div(num1, num2)
+
+	assert.Equal(t, expectedResults, err.Error())
+}
+
+func TestSortAscending(t *testing.T) {
+	numbers := []int{9, 5, 2, 7, 1, 3}
+	numbersOk := []int{1, 2, 3, 5, 7, 9}
+
+	SortAscending(numbers)
+
+	assert.Equal(t, numbersOk, numbers, "sejam iguais")
+
 }
